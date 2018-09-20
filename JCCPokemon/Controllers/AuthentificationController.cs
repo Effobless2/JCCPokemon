114eab79fe14937d84ca3ed4b984eaf820ed9284
Maxime Deboffle <maxime.deboffle@hotmail.com>
2018-09-20 22:14:11 +0200

    SignIn va vers database

--- JCCPokemon/Controllers/AuthentificationController.cs
+++ JCCPokemon/Controllers/AuthentificationController.cs
@@ -2,24 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JCCP.AuthentificationConnector;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JCCPokemon.Controllers
 {
     public class AuthentificationController : Controller
     {
+        private readonly IAuthentificationService _authentificationService;
+
+        public AuthentificationController(IAuthentificationService authentificationService)
+        {
+            _authentificationService = authentificationService;
+        }
+
         [Route("Authentification")]
-        [Route("Home/SignUp")]
-        public IActionResult SignUp()
+        [Route("Authentification/SignUp")]
+        public async Task<IActionResult> SignUp()
         {
             return View();
         }
 
         [Route("Authentification")]
-        [Route("Home/SignIn")]
-        public IActionResult SignIn()
+        [Route("Authentification/SignIn")]
+        public async Task<IActionResult> SignIn()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task Connection(string login, string password)
+        {
+            string res = await _authentificationService.UserAuthentification(login, password);
         }
     }
 }
