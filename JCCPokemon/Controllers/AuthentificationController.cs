98e57f1e6c22dc9f0a78ec2ed3f552a3081829c9
Maxime Deboffle <maxime.deboffle@hotmail.com>
2018-09-20 21:38:45 +0200

    _Layout et Authentification Liés

---
+++ JCCPokemon/Controllers/AuthentificationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JCCPokemon.Controllers
+{
+    public class AuthentificationController : Controller
+    {
+        [Route("Authentification")]
+        [Route("Home/SignUp")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [Route("Authentification")]
+        [Route("Home/SignIn")]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
