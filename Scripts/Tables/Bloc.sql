76b03fa6a417fe8a80bfa4addc1cc68e846ee936
Maxime Deboffle <maxime.deboffle@hotmail.com>
2018-10-03 18:42:23 +0200

    image d'un bloc supprimé

--- Scripts/Tables/Bloc.sql
+++ Scripts/Tables/Bloc.sql
@@ -2,6 +2,5 @@ CREATE TABLE Bloc(
 	BlocId UNIQUEIDENTIFIER PRIMARY KEY DEFAULT newsequentialid(),
 	FrenchName nvarchar(max),
 	EnglishName nvarchar(max),
-	ImageUrl nvarchar(max),
 	CreationYear date
 )
