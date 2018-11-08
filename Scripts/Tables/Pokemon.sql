5ca7a236f67d7b9bdcfe2b4e294d566127d0c303
Maxime Deboffle <maxime.deboffle@hotmail.com>
2018-11-08 08:44:43 +0100

    Ajout Pokemon.ImageUrl

--- Scripts/Tables/Pokemon.sql
+++ Scripts/Tables/Pokemon.sql
@@ -2,5 +2,6 @@ CREATE TABLE Pokemon (
 	PokemonId UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
 	NumPokedex int,
 	FrenchName NVARCHAR(max),
-	EnglishName NVARCHAR(max)
+	EnglishName NVARCHAR(max),
+	ImageUrl nvarchar(max)
 )
