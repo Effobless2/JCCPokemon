8b83e634964d31e6cc837daa802181ffb15e5280
Maxime Deboffle <maxime.deboffle@hotmail.com>
2018-11-16 22:56:21 +0100

    Gestion des erreures + prêt création PokemonAPI pour envoi au serveur

--- JCCPokemon/wwwroot/js/react/Admin/Formulars/PokemonFormular.tsx
+++ JCCPokemon/wwwroot/js/react/Admin/Formulars/PokemonFormular.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react'
 
 interface PokemonState{
-    myImage: any
+    myImage: any,
+    frenchStyle : any,
+    englishStyle : any,
+    pokedexStyle : any
 }
 
 
@@ -10,7 +13,10 @@ export default class PokemonFormular extends React.Component<{},PokemonState>{
     constructor(props) {
         super(props);
         this.state = {
-            myImage : null
+            myImage : null,
+            frenchStyle : null,
+            englishStyle : null,
+            pokedexStyle : null
         }
     }
 
@@ -26,8 +32,61 @@ export default class PokemonFormular extends React.Component<{},PokemonState>{
         reader.readAsDataURL(file);
     }
 
-    sendRequest(){
+    sendRequest = () =>{
+        let frenchNameText = document.getElementById("frenchName") as any
+        let englishNameText = document.getElementById("englishName") as any
+        let numPokedexText = document.getElementById("pokedexNumber") as any
+        let numPokedex = null;
+        numPokedex = parseInt(numPokedexText.value);
+        if (isNaN(numPokedex)){
+            this.numPokedexOnChange();
+        }
+        if (frenchNameText.value == ""){
+            this.frenchNameOnChange();
+        }
+        if (englishNameText.value == ""){
+            this.englishNameOnChange();
+        }
+        if (englishNameText.value != "" && frenchNameText.value != "" && !isNaN(numPokedex)){
+            console.log("nickel!")
+        }
 
+    }
+
+    frenchNameOnChange = () =>{
+        let frName = document.getElementById("frenchName") as any;
+        if (frName.value == ""){
+            this.setState({ frenchStyle : {border: "solid red 1px"}});
+            frName.placeholder = "Empty name is invalid";
+        } else {
+            this.setState({frenchStyle : null});
+            frName.placeholder = "Insérez le nom du Pokemon.";
+        }
+    }
+
+    englishNameOnChange = () =>{
+        
+        let enName = document.getElementById("englishName") as any;
+        if (enName.value == ""){
+            this.setState({ englishStyle : {border: "solid red 1px"}});
+            enName.placeholder = "Empty name is invalid";
+        } else {
+            this.setState({englishStyle : null});
+            enName.placeholder = "Insérez le nom du Pokemon.";
+        }
+    }
+
+    numPokedexOnChange = () =>{
+        let numT = document.getElementById("pokedexNumber") as any;
+        if (isNaN(parseInt(numT.value))){
+            this.setState({pokedexStyle : {border: "solid red 1px"}});
+            numT.value="";
+            numT.placeholder = "Veuillez insérer un entier valide.";
+        } else{
+            this.setState({pokedexStyle : null });
+            numT.placeholder = "Entrez le numéro du pokedex";
+
+        }
     }
 
 
@@ -46,8 +105,8 @@ export default class PokemonFormular extends React.Component<{},PokemonState>{
                                         type="text" 
                                         name="frenchName" 
                                         id="frenchName" 
-                                        /*onChange={this.frenchNameOnChange}*/ 
-                                        /*style={this.state.frenchStyle}*/ 
+                                        onChange={this.frenchNameOnChange} 
+                                        style={this.state.frenchStyle}
                                         className="form-control" 
                                         placeholder="Insérez le nom du Bloc"
                                     />
@@ -61,8 +120,8 @@ export default class PokemonFormular extends React.Component<{},PokemonState>{
                                         type="text" 
                                         name="englishName" 
                                         id="englishName" 
-                                        /*onChange={this.englishNameOnChange} 
-                                        style={this.state.englishStyle}*/ 
+                                        onChange={this.englishNameOnChange} 
+                                        style={this.state.englishStyle} 
                                         className="form-control" 
                                         width="50%" 
                                         placeholder="Insérez le nom Anglais du Bloc"
@@ -71,12 +130,16 @@ export default class PokemonFormular extends React.Component<{},PokemonState>{
                             </div>
 
                             <div className="row"  style={{display: "flex", alignItems: "center"}}>
-                                <h2 label-for="yearSelector" className="col-lg-6 col-xs-6">Numéro du Pokedex :</h2>
+                                <h2 label-for="pokedexNumber" className="col-lg-6 col-xs-6">Numéro du Pokedex :</h2>
                                 <div className="col-lg-6 col-xs-6">
                                     <input 
                                         type="text" 
                                         className="form-control" 
                                         placeholder="Entrez le numéro du pokedex"
+                                        id = "pokedexNumber"
+                                        name = "pokedexNumber"
+                                        onChange={this.numPokedexOnChange} 
+                                        style = {this.state.pokedexStyle}
                                     />
                                 </div>
                             </div>
@@ -107,7 +170,7 @@ export default class PokemonFormular extends React.Component<{},PokemonState>{
                                     className="btn btn-primary" 
                                     style={{marginRight:'5pt', marginTop:'5pt'}} 
                                     type="button" 
-                                    /*onClick={this.sendRequest}*/
+                                    onClick={this.sendRequest}
                                 >
                                     Créer le pokemon
                                 </button>
