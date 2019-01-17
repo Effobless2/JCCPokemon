30c4a721c152cb4c97964ff69b02d3ab08676acc
Maxime Deboffle <maxime.deboffle@hotmail.com>
2019-01-17 11:06:43 +0100

    BO.EnergyType ok

---
+++ JCCP.BO/EnergyType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JCCP.BO
+{
+    class EnergyType
+    {
+        public Guid TypeId { get; set; }
+        public string FrenchName { get; set; }
+        public string EnglishName { get; set; }
+        public string Logo { get; set; }
+    }
+}
