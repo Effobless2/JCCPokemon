838a43605867f271e5097b80d5ac24b9d0005a23
Maxime Deboffle <maxime.deboffle@hotmail.com>
2019-01-17 14:00:33 +0100

    Creation classe JCCP.BO.rarity

---
+++ JCCP.BO/Rarity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JCCP.BO
+{
+    public class Rarity
+    {
+        public Guid RarityId { get; set; } = Guid.Empty;
+        public string FrenchName { get; set; }
+        public string EnglishName { get; set; }
+        public string Logo { get; set; }
+    }
+}
