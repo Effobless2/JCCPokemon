58efa878905890dd18fd5d347eee1e3169d654b3
Maxime Deboffle <maxime.deboffle@hotmail.com>
2019-01-31 11:03:06 +0100

    Initialisation RarityFormular

--- JCCPokemon/wwwroot/js/react/Admin/App.tsx
+++ JCCPokemon/wwwroot/js/react/Admin/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import BlocFormular from './Formulars/BlocFormular';
 import SetFormular from './Formulars/SetFormular';
 import PokemonFormular from './Formulars/PokemonFormular';
+import RarityFormular from './Formulars/RarityFormular';
 
 export default class App extends React.Component<{},{content:any}>{
     constructor(props){
@@ -57,12 +58,27 @@ export default class App extends React.Component<{},{content:any}>{
         </div>});
     }
 
+    AddRarity = () =>{
+        this.setState({content : <div> 
+            <button type="button" className="close" onClick={this.CloseForm} data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <div className = "row" style={{display:"flex", alignItems:"flex-end", flexWrap:"wrap"}}>
+                <div className="col-lg-10">
+                    <RarityFormular/>
+                </div>
+                <button type="button" className="btn btn-danger" onClick={this.CloseForm}>Annuler</button>
+            </div>
+        </div>});
+    }
+
     render(){
         return (
             <div>
                 <button className="btn btn-primary" style={{"margin": "5px"}} onClick={this.AddBloc}>Add Bloc</button>
                 <button className="btn btn-primary" style={{"margin": "5px"}} onClick={this.AddSet}>Add Set</button>
                 <button className="btn btn-primary" style={{"margin": "5px"}} onClick={this.AddPokemon}>Add Pokemon</button>
+                <button className="btn btn-primary" style={{"margin": "5px"}} onClick={this.AddRarity}>Add Rarity</button>
                 <div>
                 
                     {this.state.content}
---
+++ JCCPokemon/wwwroot/js/react/Admin/Formulars/RarityFormular.tsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+export default class RarityFormular extends React.Component<any,any>{
+    
+    constructor(props : any) {
+        super(props);    
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Hello Rarity !</h1>
+            </div>
+        )
+    }
+}
