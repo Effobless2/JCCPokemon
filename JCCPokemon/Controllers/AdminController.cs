1a20fdc99e7bd59d1a4db520d92bf7f417d62d58
Maxime Deboffle <maxime.deboffle@hotmail.com>
2019-04-01 18:53:00 +0200

    Poursuite commit précédent

--- JCCPokemon/Controllers/AdminController.cs
+++ JCCPokemon/Controllers/AdminController.cs
@@ -78,77 +78,7 @@ namespace JCCPokemon.Controllers
             }
             return RedirectToAction("Authentification");
         }
-
-        [HttpPost]
-        [Authorize(Policy = "IsAdmin")]
-        public async Task<ActionResult> CreateNewExtension(List<IFormFile> logo, List<IFormFile> symbol, string frenchName, string englishName, Guid blocId)
-        {
-            Guid myguid = Guid.NewGuid();
-            string symbolUri = SendSymbol(symbol, myguid.ToString().Substring(0,5));
-            string logoUri = SendLogo(logo, myguid.ToString().Substring(0, 5));
-            Extension e = new Extension()
-            {
-                ExtensionId = myguid,
-                LogoUrl = logoUri,
-                SymbolUrl = symbolUri,
-                FrenchName = frenchName,
-                EnglishName = englishName,
-                BlocId = blocId
-            };
-
-           bool result = await _extensionService.CreateNewExtension(e);
-           if (result)
-            {
-                return Ok();
-            }
-            return NotFound();
-        }
-
-        public string SendSymbol(List<IFormFile> files, string name)
-        {
-            string baseDirectory = "images\\symbol";
-            if (!Directory.Exists(Path.Combine(_hostingEnvironment.WebRootPath, baseDirectory)))
-                Directory.CreateDirectory(Path.Combine(_hostingEnvironment.WebRootPath, baseDirectory));
-
-            string uri = "";
-            if (files.Count == 1 && files[0].Length > 0)
-            {
-                using (Stream stream = files[0].OpenReadStream())
-                {
-                    using (var fileStream = System.IO.File.Create(Path.Combine(_hostingEnvironment.WebRootPath, baseDirectory, name + "." + files[0].ContentType.Split('/').Last())))
-                    {
-                        stream.Seek(0, SeekOrigin.Begin);
-                        stream.CopyTo(fileStream);
-                        uri = Path.Combine(baseDirectory, name + "." + files[0].ContentType.Split('/').Last());
-                    }
-                }
-            }
-
-            return uri;
-        }
-
-        public string SendLogo(List<IFormFile> files, string name)
-        {
-            string baseDirectory = "images\\logo";
-            if (!Directory.Exists(Path.Combine(_hostingEnvironment.WebRootPath, baseDirectory)))
-                Directory.CreateDirectory(Path.Combine(_hostingEnvironment.WebRootPath, baseDirectory));
-
-            string uri = "";
-            if (files.Count == 1 && files[0].Length > 0)
-            {
-                using (Stream stream = files[0].OpenReadStream())
-                {
-                    using (var fileStream = System.IO.File.Create(Path.Combine(_hostingEnvironment.WebRootPath, baseDirectory, name + "." + files[0].ContentType.Split('/').Last())))
-                    {
-                        stream.Seek(0, SeekOrigin.Begin);
-                        stream.CopyTo(fileStream);
-                        uri = Path.Combine(baseDirectory, name + "." + files[0].ContentType.Split('/').Last());
-                    }
-                }
-            }
-
-            return uri;
-        }
+        
 
         [Authorize(Policy = "IsAdmin")]
         [HttpPost]
